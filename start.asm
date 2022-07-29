9f16ce2bd9ba20e86b5ce589e11434525d701532
theverygaming <clemibunge@gmail.com>
2022-07-30 01:14:12 +0200

    argv and envp fix

--- start.asm
+++ start.asm
@@ -1,20 +1,24 @@
 section .text
 global  _start
-extern _startc
+extern main
 _start:
     xor ebp, ebp ; mark end of stack frames
     
-    pop eax ; argc
-    pop ebx ; argv
-    lea ecx, [4+ebx+eax*4] ; envp_str = 4+argv+(4*argc)
-    push ecx ; envp_str
-    push ebx ; argv
-    push eax ; argc
+    mov edi, [esp] ; argc
+    lea esi, [esp+4] ; argv
+    lea edx, [esp+4+4*edi+4] ; envp_str
+    push edx ; envp
+    push esi ; argv
+    push edi ; argc
+    xor eax, eax
 
-    call _startc ; call C start function(int argc, char* argv[], char* envp_str)
+
+    call main ; call C start function(int argc, char* argv[], char* envp_str)
     
     ; exit with return value of main
     mov ebx, eax ; main returns in eax
     mov eax, 1 ; sys_exit
     int 0x80
+.loop
     hlt ; if we are here something went wrong badly
+    jmp .loop
