9d0274cb2a22a139a91147766c40285fe3517a16
theverygaming <clemibunge@gmail.com>
2022-08-12 01:19:33 +0200

    arguments, boop and exit

--- main.c
+++ main.c
@@ -4,63 +4,60 @@
 
 bool shell_running = true;
 
-static void replace_char(char* str, size_t len,  char c, char r) {
+static void replace_chars(char* str, size_t len,  char c, char r) {
     while(len--) {
         if(*str == c) {
             *str = r;
-            return;
         }
         str++;
     }
 }
 
-static int str_remove_until(char* out, const char* strin, char c) {
-    bool remove = true;
-    while(*strin) {
-        if(!remove) {
-            *out = *strin;
-            out++;
-            strin++;
-            continue;
-        }
-        if(*strin == c) { remove = false; }
-        strin++;
-    }
-    if(remove) { return -1; }
-    return 0;
-}
-
-static void create_charptrptr(char* in, char** out) {
-    size_t count = 0;
-    while(true) {
-        size_t strl = strlen(in);
-        if(strl != 0) {
-            out[count] = in;
+static int countc(const char* str, char c) {
+    int count = 0;
+    while(*str) {
+        if(*str++ == c) {
             count++;
-            in += strl+1;
-            continue;
         }
-        break;
-    }
-    
-}
-
-static size_t create_charptrptr_count(char* in) {
-    size_t count = 0;
-    while(true) {
-        size_t strl = strlen(in);
-        if(strl != 0) {
-            count++;
-            in += strl+1;
-            continue;
-        }
-        break;
     }
     return count;
 }
 
-void run_cmd(char** cmd, char** argv) {
+bool run_internal_cmd(char* cmd, char* argv[]) {
+    int argc = 0;
+    while(argv[argc]) {
+        argc++;
+    }
+    
+    if(!strcmp(cmd, "boop")) {
+        if(argc > 1) {
+            sys_write(1, "*", 1);
+            if(!strcmp(argv[1], "me")) {
+                sys_write(1, "boops you", 9);
+            }
+            else {
+                sys_write(1, argv[1], strlen(argv[1]));
+                sys_write(1, " boops back", 11);
+            }
+            sys_write(1, "*", 1);
+            if(!strcmp(argv[1], "me")) {
+                sys_write(1, " :3", 3);
+            }
+        }
+        else {
+            sys_write(1, ":flushed:", 10);
+        }
+        sys_write(1, "\n", 1);
+        return true;
+    }
 
+    if(!strcmp(cmd, "exit")) {
+        sys_write(1, "https://tenor.com/view/crying-emoji-dies-gif-21956120\n", 54);
+        shell_running = false;
+        return true;
+    }
+
+    return false;
 }
 
 int main(int argc, char* argv[], char* envp[]) {
@@ -78,23 +75,43 @@ int main(int argc, char* argv[], char* envp[]) {
     }
 
     sys_write(1, "welcome to the shitshell:tm:\n", 29);
+
     char input_buf[100];
-    char args_buf[100];
+
     while(shell_running) {
         sys_write(1, "# ", 2);
         sys_read(1, input_buf, sizeof(input_buf));
-        replace_char(input_buf, sizeof(input_buf), '\n', '\0');
-        str_remove_until(args_buf, input_buf, ' ');
-        replace_char(args_buf, sizeof(args_buf), ' ', '\0');
+        replace_chars(input_buf, sizeof(input_buf), '\n', '\0');
+
+        /* create argv array, null terminated */
+        memcpy(input_buf, input_buf, strlen(input_buf));
+        int arg_count = countc(input_buf, ' ') + 1; // count spaces in string
+        char *arg_arr[arg_count +1];
+        arg_arr[arg_count] = 0; // null termination
+        char *strptr = input_buf;
+        for(int i = 0; i < arg_count; i++) {
+            arg_arr[i] = strptr;
+            while(*strptr && *strptr != ' ') {
+                *strptr++;
+            }
+            *strptr++;
+        }
+        replace_chars(input_buf, sizeof(input_buf), ' ', '\0');
+
+        if(run_internal_cmd(arg_arr[0], arg_arr)) {
+            continue;
+        }
         
         pid_t forked = sys_fork();
         if(!forked) {
-            sys_execve(input_buf, 0, 0);
+            sys_execve(arg_arr[0], arg_arr, 0);
             sys_write(1, "execve failed!\n", 15);
             return 1; // execve failed
         }
         for(uint32_t i = 0; i < 0xFFFFFF; i++) {}
         sys_waitpid(forked, 0, 0);
+
+        memset(input_buf, 0, 100); // clear buffer
     }
 
     return 0;
