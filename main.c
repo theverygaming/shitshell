9e2bb4d35e959008c2044bfff598279e3eb2dc0b
theverygaming <theverygaming@users.noreply.github.com>
2025-04-02 07:14:33 +0200

    fix stuff

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 static void replace_chars(char *str, size_t len, char c, char r) {
     while (len--) {
